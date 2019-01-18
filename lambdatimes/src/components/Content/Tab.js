25fc4b2e1110dbb3c0d27886f5ff80b382c03f69
Estevan <estevan.rodriguez10@yahoo.com>
2019-01-18 09:58:10 -0800

    rendered tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -8,9 +8,6 @@ const Tab = props => {
     <div
       className={props.selectedTab === props.tab ? 'tab active-tab':'tab'}
       onClick={() => props.selectTabHandler(props.tab)}
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      
     >
       {props.tab.toUpperCase()}
     </div>
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,10 +5,7 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-      </div>
-      {props.tabs.map((tab)=> {
+        {props.tabs.map((tab)=> {
                 return( 
                   
                   <div>
@@ -22,6 +19,8 @@ const Tabs = props => {
                 </div>
                 );
             })}
+      </div>
+      
     </div>
   );
 };
