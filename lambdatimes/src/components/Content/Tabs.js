882ec99917b4f193a0c76400c4c98de9d4c5d5b1
Estevan <estevan.rodriguez10@yahoo.com>
2019-01-18 09:41:57 -0800

    rendered cards to screen

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,6 +8,20 @@ const Tabs = props => {
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
+      {props.tabs.map((tab)=> {
+                return( 
+                  
+                  <div>
+                    
+                <Tab 
+                
+                tab = {tab}
+                selectTabHandler = {props.selectTabHandler}
+                selectedTab={props.selectedTab}
+                />
+                </div>
+                );
+            })}
     </div>
   );
 };
