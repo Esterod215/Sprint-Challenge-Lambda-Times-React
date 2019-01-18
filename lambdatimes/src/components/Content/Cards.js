03fbd28e9e43ea1b9e9abd584bed1f6c05c7c2f8
Estevan <estevan.rodriguez10@yahoo.com>
2019-01-18 10:38:13 -0800

    added propTypes

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
@@ -19,5 +20,8 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propType={
+  cards: PropTypes.arrayOf(PropTypes.string)
+}
 
 export default Cards;
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -27,6 +27,7 @@ export default class Content extends Component {
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
     this.setState({selected:tab})
+    console.log({tab});
   };
 
   filterCards = () => {
@@ -42,7 +43,15 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+   if(this.state.selected ==='all'){
+    return this.state.cards;}
+    else{
+      this.state.cards.filter(card=>{
+        if(this.state.selected === this.state.tabs.tab){
+          return card;
+        }
+      })
+    }
   };
 
   render() {
